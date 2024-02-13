6d1b89797614648bb479b2f78e8760fe26bbbf08
Muxaaa375 </>
2024-02-13 14:42:42 +0500

    Разработанный класс отправки сообщения на почту. Паксюаткин

---
+++ Classes/SendMail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace RegIN_Паксюаткин.Classes
+{
+    public class SendMail
+    {
+        public static void SendMessage(string Message, string To)
+        {
+
+            var smtpClient = new SmtpClient("smtp.yandex.ru")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("yandex@yandex.ru", "password"),
+                EnableSsl = true,
+            };
+            smtpClient.Send("landaxer@yandex.ru", To, "Проект RegIn", Message);
+        }
+    }
+}
